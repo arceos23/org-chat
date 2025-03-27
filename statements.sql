355b05569010675fed5c597446ee871fff5ffe99
Shane Arceo <arceoshane23@gmail.com>
2025-03-26 20:55:50 -0400

    updated schema to have unique values in the title and organization tables

--- statements.sql
+++ statements.sql
@@ -1,11 +1,11 @@
 CREATE TABLE title (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(1024) NOT NULL
+    name VARCHAR(1024) UNIQUE NOT NULL
 );
 
 CREATE TABLE organization (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(1024) NOT NULL
+    name VARCHAR(1024) UNIQUE NOT NULL
 );
 
 CREATE TABLE person (
