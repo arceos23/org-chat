8701661eea8fb84cee62b93952b069ef15b1a6fd
Shane Arceo <arceoshane23@gmail.com>
2025-03-26 20:27:09 -0400

    created sql tables

---
+++ statements.sql
@@ -0,0 +1,17 @@
+CREATE TABLE title (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE organization (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE person (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(1024) NOT NULL,
+    title_id UUID REFERENCES title(id),
+    organization_id UUID REFERENCES organization(id),
+    about TEXT
+);
